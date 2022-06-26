90bd71834fddb9ad4f3b122da5b12b4f69db8382
Xiaofei Du <xiaofei.du@instill.tech>
2022-06-26 15:56:06 +0100

    fix: remove normalizing session

--- session/session.go
+++ session/session.go
@@ -10,32 +10,6 @@ import (
 	usagePB "github.com/instill-ai/protogen-go/vdp/usage/v1alpha"
 )
 
-// contains checks if a string is present in a slice
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-	return false
-}
-
-var (
-	DefaultSessionEdition = "local-ce:dev"
-	ValidSessionEditions  = []string{"local-ce", DefaultSessionEdition}
-)
-
-// NormSessionEdition normalises a session edition input.
-// If it's not valid, then `DefaultSessionEdition` is returned, or else the original edition input is returned.
-func NormSessionEdition(edition string) string {
-
-	if valid := contains(ValidSessionEditions, edition); valid {
-		return edition
-	} else {
-		return DefaultSessionEdition
-	}
-}
-
 // Session is a new type of usagePB.session
 type Session usagePB.Session
 
